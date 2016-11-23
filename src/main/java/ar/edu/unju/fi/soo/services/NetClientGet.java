c105af4c962979bb3b39101c79abbca1125c7f94
Deb Sudañez <deb.sudaez@gmail.com>
2016-11-23 03:42:07 -0300

    Created Implement for NetClientGet and added chageToDollar method

--- src/main/java/ar/edu/unju/fi/soo/services/NetClientGet.java
+++ src/main/java/ar/edu/unju/fi/soo/services/NetClientGet.java
@@ -1,50 +1,7 @@
 package ar.edu.unju.fi.soo.services;
 
-import org.springframework.transaction.annotation.Transactional;
-import javax.inject.Named;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-@Named
-@Transactional
-public class NetClientGet {
-	public String getCurrencyValue() {
-		String output = null;
-		  try {
-
-				URL url = new URL("http://currencies.apps.grandtrunk.net/getlatest/usd/ars");
-				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestMethod("GET");
-				conn.setRequestProperty("Accept", "application/json");
-
-				if (conn.getResponseCode() != 200) {
-					throw new RuntimeException("Failed : HTTP error code : "
-							+ conn.getResponseCode());
-				}
-
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-					(conn.getInputStream())));
-
-				
-				while ((output = br.readLine()) != null) {
-					System.out.println(output);
-				}
-				conn.disconnect();
-		
-			  } catch (MalformedURLException e) {
-
-				e.printStackTrace();
-
-			  } catch (IOException e) {
-
-				e.printStackTrace();
-
-			  }
-		  return output;
-	}
+public interface NetClientGet {
+	String getCurrencyValue();
+	Double convertToDollar(double value);
 }
---
+++ src/main/java/ar/edu/unju/fi/soo/services/impl/NetClientGetImpl.java
@@ -0,0 +1,60 @@
+package ar.edu.unju.fi.soo.services.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.inject.Named;
+
+import org.springframework.transaction.annotation.Transactional;
+import ar.edu.unju.fi.soo.services.NetClientGet;
+
+@Named
+@Transactional
+public class NetClientGetImpl implements NetClientGet {
+	@Override
+	public String getCurrencyValue() {
+		String output = null;
+		 try {
+
+				URL url = new URL("http://currencies.apps.grandtrunk.net/getlatest/usd/ars");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+				
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+				conn.disconnect();
+		
+			  } catch (MalformedURLException e) {
+
+				e.printStackTrace();
+
+			  } catch (IOException e) {
+
+				e.printStackTrace();
+
+			  }
+		  return output;
+		 
+	}
+
+	@Override
+	public Double convertToDollar(double value) {
+		double currency = Double.parseDouble(getCurrencyValue());
+		return value * currency;
+	}
+}
