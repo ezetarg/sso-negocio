6f8a2de9fe7132bed778f5717568038c4d9786c6
Ezequiel Montes <emontes@theinit.com>
2016-11-23 11:23:44 -0300

    Add service methods to get and pay a fee.

--- src/main/java/ar/edu/unju/fi/soo/services/AgencyService.java
+++ src/main/java/ar/edu/unju/fi/soo/services/AgencyService.java
@@ -3,12 +3,17 @@ package ar.edu.unju.fi.soo.services;
 import java.util.List;
 
 import ar.edu.unju.fi.soo.model.Client;
+import ar.edu.unju.fi.soo.model.Fee;
 import ar.edu.unju.fi.soo.model.Plan;
 import ar.edu.unju.fi.soo.model.Vehicle;
 
 public interface AgencyService {
 
 	List<Plan> findPlanByClientName(String clientName);
+
+	Fee getNextUnpaidFee(Long planId);
+
+	void payNextFee(Long planId);
 
 	void savePlan(Plan plan);
 
--- src/main/java/ar/edu/unju/fi/soo/services/impl/AgencyServiceImpl.java
+++ src/main/java/ar/edu/unju/fi/soo/services/impl/AgencyServiceImpl.java
@@ -8,18 +8,22 @@ import javax.inject.Named;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.unju.fi.soo.model.Client;
+import ar.edu.unju.fi.soo.model.Fee;
 import ar.edu.unju.fi.soo.model.Plan;
 import ar.edu.unju.fi.soo.model.Vehicle;
 import ar.edu.unju.fi.soo.model.dao.ClientDAO;
+import ar.edu.unju.fi.soo.model.dao.FeeDAO;
 import ar.edu.unju.fi.soo.model.dao.PlanDAO;
 import ar.edu.unju.fi.soo.model.dao.VehicleDAO;
 import ar.edu.unju.fi.soo.services.AgencyService;
 
 @Named
-@Transactional
+@Transactional(readOnly = true)
 public class AgencyServiceImpl implements AgencyService {
 	@Inject
 	private ClientDAO clientDAO;
+	@Inject
+	private FeeDAO feeDAO;
 	@Inject
 	private PlanDAO planDAO;
 	@Inject
@@ -31,23 +35,36 @@ public class AgencyServiceImpl implements AgencyService {
 	}
 
 	@Override
+	public Fee getNextUnpaidFee(Long planId) {
+		return feeDAO.getNextUnpaidFee(planId);
+	}
+
+	@Override
+	@Transactional
+	public void payNextFee(Long planId) {
+		Fee fee = getNextUnpaidFee(planId);
+		fee.pay();
+		feeDAO.save(fee);
+	}
+
+	@Override
+	@Transactional
 	public void savePlan(Plan plan) {
 		planDAO.save(plan);
 	}
 
 	@Override
+	@Transactional
 	public void saveClient(Client client) {
 		clientDAO.save(client);
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public List<Vehicle> listVehicles() {
 		return vehicleDAO.list();
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public List<Client> listClients() {
 		return clientDAO.list();
 	}
